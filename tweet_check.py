405799180a6da17e537a673a08e6e19d515dfb29
Venkatesh <suvarna_venkatesh@hotmail.com>
2017-11-24 16:14:07 +0530

    Update tweet_check.py

--- tweet_check.py
+++ tweet_check.py
@@ -4,10 +4,10 @@ from tweepy.streaming import StreamListener
 
 
 #consumer key, consumer secret, access token, access secret.
-ckey="Rf5pDiFPAni0KCMfCsMe6c4Us"
-csecret="LtXzxzSSHSsHNVv9gb41EfDLnjVs4rFBK5Ss2pj3Aey0JK172J"
-atoken="162992083-lKyCwKLsOw8KWbD8UE6rvnvhsFtwOFrZYSUSC0sZ"
-asecret="OAbgxXlpIHwgoiQUxBuzfy6ckp5Cd33iXe0HBSCq2DPd4"
+ckey=""
+csecret="
+atoken=""
+asecret=""
 
 class listener(StreamListener):
 
